6e7e53893b6559e97fac359271420c8cef5fc1b0
sarath <sarathkrishnan2332@gmail.com>
2022-10-16 00:27:25 +0530

    first commit of photogram

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>Photogram</title>
+     <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  </head>
+  <body>
+    
+    <?php include '_templates/header.php';?>
+    <?php include '_templates/calltoaction.php';?>
+    <?php include '_templates/main.php';?>
+    <?php include '_templates/footer.php';?>
+    
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+ 
+  </body>
+</html>
